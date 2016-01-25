91179906542e7c7931827c32a547485fdf4441d5
liujianping <liujianping@liujianpingdeMacBook-Pro.local>
2016-01-25 13:56:05 +0800

    fix

--- insert.go
+++ insert.go
@@ -23,24 +23,24 @@ func (insert *InsertSQL) Statment() *Statment {
 
 	fmts := []string{}
 	vals := []interface{}{}
+	columns := []string{}
+	symbols := []string{}
+	for _, col := range insert.table.Values() {
+		columns = append(columns, col.Name())
+		symbols = append(symbols, "?")
+		vals = append(vals, col.Get())
+	}
 
 	fmts = append(fmts, "INSERT")
 	fmts = append(fmts, "INTO")
 	fmts = append(fmts, insert.table.SQL())
 	fmts = append(fmts, "(")
-	fmts = append(fmts, insert.table.ColumnsSQL())
+	fmts = append(fmts, strings.Join(columns, ", "))
 	fmts = append(fmts, ")")
 	fmts = append(fmts, "VALUES")
 	fmts = append(fmts, "(")
-
-	symbols := []string{}
-	for _, col := range insert.table.Values() {
-		symbols = append(symbols, "?")
-		vals = append(vals, col.Get())
-	}
 	fmts = append(fmts, strings.Join(symbols, ", "))
 	fmts = append(fmts, ")")
-
 	return &Statment{
 		format: strings.Join(fmts, " "),
 		values: vals,
